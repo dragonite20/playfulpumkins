47a913bd29b47d07af7f3e9f63c61ce674e56670
Katarina <kat9234@gmail.com>
2018-10-28 14:03:28 -0700

    photo page html css routing, information component layout fix

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ManualEntryComponent } from './manual-entry/manual-entry.component'
 import { InformationComponent } from './information/information.component'
 import { PurchaseConfirmationComponent } from './purchase-confirmation/purchase-confirmation.component'
+import { PhotoComponent } from './photo/photo.component'
 
 export const appRoutes: Routes = [
+  { path: 'photo', component: PhotoComponent },
   { path: 'purchase-confirmation', component: PurchaseConfirmationComponent },
   { path: 'information', component: InformationComponent },
   { path: 'manual-entry', component: ManualEntryComponent },
-  { path: '**', redirectTo: '/manual-entry', pathMatch: 'full'}
+  { path: '**', redirectTo: '/photo', pathMatch: 'full'}
 ];
